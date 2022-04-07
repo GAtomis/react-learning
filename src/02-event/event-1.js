603c217b2673792e5bc07ae8dc2f8dd9a82c2c71
Gavin <gavin.zhou@w-group.com.cn>
2022-04-07 10:46:20 +0800

    feat@event-1&DOC

---
+++ src/02-event/event-1.js
@@ -0,0 +1,31 @@
+/*
+ * @Description: 事件
+ * @Author: Gavin
+ * @Date: 2022-04-06 13:05:16
+ * @LastEditTime: 2022-04-06 15:34:21
+ * @LastEditors: Gavin
+ */
+
+import React, { Component } from 'react'
+
+export default class event extends Component {
+  render() {
+    return (
+      <div>
+
+
+        <input />
+        <button onClick={() => {
+          console.log("出发");
+        }}>add</button>
+        <button onClick={this.handleClick}>add2</button>
+      </div>
+    )
+  }
+  handleClick = () => {
+
+    console.warn(1233);
+
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Description: react入口文件
  * @Author: Gavin
  * @Date: 2022-04-02 23:47:53
- * @LastEditTime: 2022-04-06 12:30:35
+ * @LastEditTime: 2022-04-06 15:34:54
  * @LastEditors: Gavin
  */
 import React from 'react'
@@ -12,5 +12,6 @@ import ReactDom from 'react-dom'
 // import App from './01-base/FP-components'
 
 // import App from './01-base/child-components'
-import App from './01-base/style-components'
+// import App from './01-base/style-components'
+import App from './02-event/event-1'
 ReactDom.render(<App></App>,document.getElementById('root'))
